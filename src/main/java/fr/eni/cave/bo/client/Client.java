361a2b8b8a31526f13cc6ba91a44b55f08990275
Valentin <valent.chamboredon@gmail.com>
2026-01-06 14:30:20 +0100

    tp fil rouge ENI

---
+++ src/main/java/fr/eni/cave/bo/client/Client.java
@@ -0,0 +1,30 @@
+package fr.eni.cave.bo.client;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="CAV_CLIENT")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"pseudo"})
+@Builder
+public class Client {
+
+    @Id
+    @Column(name = "LOGIN", length = 255, nullable = false, unique = true)
+    private String pseudo;
+
+    @ToString.Exclude
+    @Column(name = "PASSWORD", length = 68)
+    private String password;
+
+    @Column(name = "LAST_NAME", length = 90)
+    private String nom;
+
+    @Column(name = "FIRST_NAME", length = 150)
+    private String prenom;
+}
---
+++ src/test/java/fr/eni/cave/repository/ClientRepositoryTest.java
@@ -0,0 +1,72 @@
+package fr.eni.cave.repository;
+
+import fr.eni.cave.bo.client.Client;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@Slf4j
+class ClientRepositoryTest {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Test
+    @DisplayName("Crée un client, save et vérif qu'il existe en base")
+    public void test_createClient_save_checkIfExists(){
+
+        Client client = Client
+                .builder()
+                .pseudo("pseudo")
+                .password("password")
+                .nom("nom")
+                .prenom("prenom")
+                .build();
+        log.info("-------------------------");
+        log.info("Create Client :{}", client);
+        log.info("-------------------------");
+        clientRepository.save(client);
+
+        assertTrue(clientRepository.existsById(client.getPseudo()));
+    }
+
+    @Test
+    @DisplayName("Crée un client, delete le et vérif qu'il n'existe plus en base")
+    public void test_createClient_save_delete_checkIfDoesntExists(){
+
+        Client client = Client
+                .builder()
+                .pseudo("pseudo2")
+                .password("password2")
+                .nom("nom2")
+                .prenom("prenom2")
+                .build();
+        log.info("-------------------------");
+        log.info("Create Client :{}", client);
+        log.info("-------------------------");
+        clientRepository.save(client);
+        log.info("-------------------------");
+        log.info("Delete Client :{}", client);
+        log.info("-------------------------");
+        clientRepository.deleteById(client.getPseudo());
+
+        assertFalse(clientRepository.existsById(client.getPseudo()));
+    }
+
+    @Test
+    @DisplayName("Vérifie que deux clients avec le même pseudo sont considérés comme égaux")
+    public void test_clientEquality_basedOnPseudoOnly() {
+        Client c1 = Client.builder().pseudo("unique").nom("NomA").build();
+        Client c2 = Client.builder().pseudo("unique").nom("NomB").build();
+
+        assertEquals(c1, c2);
+    }
+
+}
